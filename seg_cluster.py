97c3bf4f321ba0d2a328b0482f7fb781b26c5b1d
Samuel Stettner <samuel.stettner@dlr.de>
2019-06-13 11:50:06 +0200

    update

--- seg_cluster.py
+++ seg_cluster.py
@@ -19,7 +19,7 @@ plt.show()
 pic_n = pic.reshape(pic.shape[0]*pic.shape[1], pic.shape[2])
 pic_n.shape
 
-kmeans = KMeans(n_clusters=5, random_state=0).fit(pic_n)
+kmeans = KMeans(n_clusters=10, random_state=0).fit(pic_n)
 pic2show = kmeans.cluster_centers_[kmeans.labels_]
 
 cluster_pic = pic2show.reshape(pic.shape[0], pic.shape[1], pic.shape[2])
